6e74e77dab9fbce371ba96b4ab6ab0503bd386cb
Ruslan <ruslanalmasovich@gmail.com>
2024-11-05 11:25:12 +0300

    Add files via upload

--- module_6_2.py
+++ module_6_2.py
@@ -1,10 +1,12 @@
-class Sedan:
+class Vehicle:
+    # Допустимые цвета для всех видов транспорта
     ALLOWED_COLORS = {'blue', 'red', 'green', 'black', 'white'}
 
     def __init__(self, owner, model, color, mileage):
         self.owner = owner
         self.model = model
         self.mileage = mileage
+
         self.set_color(color)
 
     def set_color(self, color):
@@ -21,6 +23,12 @@ class Sedan:
         print(f"Цвет: {self.color}")
         print(f"Пробег: {self.mileage} км")
 
+# Класс Sedan, наследующий от Vehicle
+class Sedan(Vehicle):
+    def __init__(self, owner, model, color, mileage):
+        # Вызываем инициализацию базового класса
+        super().__init__(owner, model, color, mileage)
+
 vehicle1 = Sedan('Fedos', 'Toyota Mark II', 'blue', 500)
 
 vehicle1.print_info()
@@ -29,5 +37,5 @@ vehicle1.set_color('Pink')   # Нельзя сменить цвет на Pink
 vehicle1.set_color('BLACK')  # Цвет изменится на BLACK
 vehicle1.owner = 'Vasyok'    # Меняем владельца
 
-# Проверяем, что поменялось
+
 vehicle1.print_info()
